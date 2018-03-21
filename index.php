56d41d664dcc0f1eaed5b2b544c3e4cc51eb9c47
Pankaj Swami <yusufs@iprogrammer.com>
2018-03-21 17:41:16 +0530

    Added jQuery UI lib again for autocomplete

--- index.php
+++ index.php
@@ -155,7 +155,7 @@ $page = empty($_POST['page']) ? 1 : $_POST['page'];
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-		<!-- <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> -->
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function(){
 
